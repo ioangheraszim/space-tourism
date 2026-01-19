26945b7951baaffe6a37d85604a1409d310d08b2
ioangheraszim <gijohny17@gmail.com>
2026-01-19 18:00:22 +0200

    feat: finished crew page, and made some new utilities - Implemented HTML structure for crew and technology page - Finished styling crew page - Added additional utilities for buttons and fonts

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+// Variables
+
+// Navbar Variables
 const btnOpen = document.querySelector(".btn-open");
 const btnClose = document.querySelector(".btn-close");
 const navList = document.querySelector(".nav__list");
